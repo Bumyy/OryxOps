3bdaaf10afe55b187354b5c6fcffe6aa1152edbd
sunnyAYu <anandayush963@gmail.com>
2026-06-30 00:04:12 +0530

    build: trigger redeployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+// Trigger deployment update
 import { useState, useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "./store/hooks";
